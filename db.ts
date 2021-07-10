f94ea197a3117cbdf99eb3b34f0ea91136c925c5
fkwsurm <guswl543210@gmail.com>
2021-07-11 04:50:09 +0900

    v1

--- db.ts
+++ db.ts
@@ -1,13 +1,13 @@
 import mysql from 'mysql2/promise';  //promise를 쓰는 이유는 에러발생 시, 에러를 잡기 위해
 import 'dotenv/config'
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env || 'ERROR';
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
-  user: "root",
-  password: "1111",
-  database: "tododb"
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE
 });
 
 export default pool;
